63ffe636945b7139c1ea300102c67608d9a600c4
Jonathan Rovieri <jrovieri@gmail.com>
2021-01-04 15:56:26 -0300

    get /orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> orders = service.findAll();
+		return ResponseEntity.ok(orders);
+	}
+}
