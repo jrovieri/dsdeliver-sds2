02e10c548ac8a72d43fe636d38d9231341051583
Jonathan Rovieri <jrovieri@gmail.com>
2021-01-06 17:40:53 -0300

    ProductCard added

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductList from './ProductList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div>
       <StepsHeader />
+      <ProductList products={products}/>
     </div>
   )
 }
